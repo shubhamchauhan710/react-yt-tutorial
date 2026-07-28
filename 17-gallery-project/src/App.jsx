4f25e678b5c74b1f0c2154550b21097471d9adce
shubhamchauhan710 <subhamchauhan280@gmail.com>
2026-07-28 16:17:23 +0530

    Added new files

---
+++ 17-gallery-project/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+
+  const getData =()=>{
+    
+  }
+  return (
+    <div className='bg-black h-screen p-4 text-white'>
+      <button className='bg-green-600 active:scale-95 mb-3 px-5 py-2 text-white'>get data</button>
+      </div>
+  )
+}
+
+export default App
